0b43be43bbf9ebdc842d1fbb8cac82ea5cec56f6
LuanComputacao <luanengcomputacao@gmail.com>
2015-04-04 03:18:28 +0000

    erro do tutoria

--- PygLatin.py
+++ PygLatin.py
@@ -6,8 +6,8 @@ if len(original) > 0 and original.isalpha():
     print original
     word = original.lower()
     first = word[0]
-    new_word = word + first + pyg
-    new_word = new_word[1:len(new_word)]
+    last = word[len(word)]
+    new_word = last + word[1:(len(word) - 1)] + last + pyg
     print new_word
 else:
     print 'empty'
